a7f7947d3b006de42095421d5d96eadf7428a815
cafed00d <aditya.d@hotmail.com>
2014-11-19 11:47:03 -0800

    works for one variable. need to write read_input() and write_output() logic

--- HW3/logicwithoutyield.py
+++ HW3/logicwithoutyield.py
@@ -3,15 +3,18 @@
 
 from collections import namedtuple
 from string import ascii_lowercase
+from string import ascii_uppercase
 
 Rule = namedtuple('Rule', ['lhs', 'rhs'])
+
 
 r1 = Rule(lhs=['HasSymptom(x,Diarrhea)'], rhs='LostWeight(x)')
 r2 = Rule(lhs=['LostWeight(x)', 'Diagnosis(x,LikelyInfected)'], rhs='Diagnosis(x,Infected)')
 r3 = Rule(lhs=['HasTraveled(x,Tiberia)', 'HasFever(x)'], rhs='Diagnosis(x,LikelyInfected)')
-kb ={}
-kb['Rules'] = [r1,r2,r3]
-kb['Facts'] = ["HasTraveled(John,Tiberia)", "HasFever(John)", "HasSymptom(John,Diarrhea)"]
+kb1 ={}
+kb1['Rules'] = [r1,r2,r3]
+kb1['Facts'] = ["HasTraveled(John,Tiberia)", "HasFever(John)", "HasSymptom(John,Diarrhea)"]
+inferred = []
 
 def args(x):
     # chcek if x == Knows(jo, jan)
@@ -102,14 +105,51 @@ def subst(theta, q):
     ##    'Evil(x,u,i,o)'
     ##    >>> subst(th,q)
     ##    'Evil(x,u,Jo,o)'
+    print 'subbing ', theta, q
     argsq = args(q)
     opq = op(q)
     for each in theta:
         argsq [argsq.index(each)] = theta[each]
     arg_str = ','.join(argsq)
     result = opq + '(' + arg_str + ')' 
-    print 'subbing ', theta, q, ' ---> ', result
     return result
+
+
+def is_constant(x):
+    return x[0] in ascii_uppercase
+
+def equal(x, y):
+    if is_constant(x) and is_constant(y):
+        return x == y
+    else:
+        return True
+
+def check_args_match(list1, list2):
+    i = 0
+    while i < len(list1):
+        each1 = list1[i]
+        each2 = list2[i]
+        if not equal(each1, each2):
+            return False
+        i += 1
+    return True
+
+
+### FIXE Matches... using this inside fetch_rules if conditon causes lot of failure printslbut gives corrct output
+def matches(goal, candidate):
+    # BUG MAJOR: Fails on mathces('Knows(Jon, Amy)', 'Knows(x,y)')
+    # Diagnos(x,Infected) matches rule r2 LostWeight(x)&Diagnosis(x,LikelyInfected)=>Diagnosis(x,Infected)
+    # but Diagnois(x,Infected) shoudlnt match r3 HasTraveled(x,Tiberia)&HasFever(x)=>Diagnosis(x,LikelyInfected)
+    gname = op(goal)
+    goal_arg = get_args(goal)
+    cand_arg = get_args(candidate)
+    if gname not in candidate:
+        return False
+    elif len(goal_arg) != len(cand_arg):
+        return False
+    else:
+        return check_args_match(goal_arg, cand_arg)
+       
 
 def fetch_rules_for_goal(KB, goal):
     # goal is of the form 'LostWeight(John)'
@@ -125,17 +165,14 @@ def fetch_rules_for_goal(KB, goal):
     garg = get_args(goal)
     rules = []
     for rule in KB['Rules']:
-        if gname in rule.rhs:
-            yield rule
-    return
+        if matches(goal, rule.rhs):
+            rules.append(rule)
+    return rules
 
 #            rules.append(rule)
 #    if not rules:
 #        rules.append(goal)
 #    return rules
-
-f1 = fetch_rules_for_goal(kb, 'LostWeight(x)')
-f2 = f = fetch_rules_for_goal(kb, 'HasSymptom(x,Diarrhea)')
 
 # FIXME: above
 # fetch_rules_for_goal(kb, 'HasSymptom(John, Diarrhea)')
@@ -150,37 +187,55 @@ def fol_bc_or(KB, goal, theta):
 ##    if goal in facts:
 ##        print goal, ' is present in KB as fact'
 ##        yield theta
+    if theta == 'failure':
+        return 'failure'
     for rule in fetch_rules_for_goal(KB, goal):
         # Standardize variables
         # For hw3 we assume there is onyl one variable
         lhs = rule.lhs
         rhs = rule.rhs
         u = unify(rhs, goal, theta)
-        for theta_prime in fol_bc_and(KB, lhs, u):
-            yield theta_prime
-    if goal in kb['Facts']:
-        yield theta
+        if u == 'failure':
+            return 'failure'
+        theta_prime = {}
+        for tp, val in enumerate(fol_bc_and(KB, lhs, u)):
+            theta_prime[tp] = val
+    if goal in KB['Facts']:
+        return theta
     else:
-        yield 'failure'
+        return 'failure'
 
 def fol_bc_and(KB, goals, theta):
     if theta == 'failure':
         return
     elif len(goals) == 0:
-       yield theta
+        return theta
     else:
+        print goals
         f = first_sentence(goals)
         r = rest_sentence(goals)
         s = subst(theta, f)
-        for theta_prime in fol_bc_or(KB, s, theta):
-            for theta_double_prime in fol_bc_and(KB, r, theta_prime):
-                yield theta_double_prime
+        theta_prime = {}
+        for tp, val in enumerate(fol_bc_or(KB, s, theta)):
+            theta_prime[tp] = val
+            theta_double_prime = {}
+            if r:
+                for tpp, valpp in enumerate(fol_bc_and(KB, r, tp)):
+                    theta_double_prime[tpp] = valpp
+            return theta_double_prime
+        return theta_prime
 
 def fol_bc_ask(KB, query, theta):
+    import pdb; pdb.set_trace()
     return fol_bc_or(KB, query, theta)
 
-theta = {}
-f = fol_bc_ask(kb, 'Diagnosis(John, Infected)', theta)
+##theta = {}
+##f = fol_bc_ask(kb1, 'Diagnosis(John, Infected)', theta)
+##
+##theta2 = {}
+##kb2 = {'Rules': [Rule(lhs=['American(x)', 'Weapon(y)', 'Sells(x,y,z)', 'Hostile(z)'], rhs='Criminal(x)'), Rule(lhs=['Missile(y)', 'Owns(Nono,y)'], rhs='Sells(West,y,Nono)'), Rule(lhs=['Missile(y)'], rhs='Weapon(y)'), Rule(lhs=['Enemy(z,America)'], rhs='Hostile(z)')], 'Facts': ['American(West)', 'Enemy(Nono,America)', 'Owns(Nono,M)', 'Missile(M)']}
+##f2 = fol_bc_ask(kb2,'Criminal(West)', theta2)
+
 
 ##th = {}
 ##unify('Knows(John,x)', 'Knows(y,Bill)', th)
